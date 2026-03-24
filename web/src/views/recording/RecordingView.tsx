ff5e241b457db8ef1d9f0344cbe7f9802eda06be
Игорь Владимирович <i.kushch@ad.spetpro.ru>
2026-03-24 15:02:20 +1100

    i18n workflow improvements and tweaks

--- web/src/views/recording/RecordingView.tsx
+++ web/src/views/recording/RecordingView.tsx
@@ -704,7 +704,7 @@ export function RecordingView({
                   value="timeline"
                   aria-label={t("timeline.aria")}
                 >
-                  <div className="">{t("timeline")}</div>
+                  <div className="">{t("timeline.label")}</div>
                 </ToggleGroupItem>
                 <ToggleGroupItem
                   className={`${timelineType == "events" ? "" : "text-muted-foreground"}`}
