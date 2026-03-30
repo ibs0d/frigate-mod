1270ab51a2350745050b05d030ce161dae17303d
Игорь Владимирович <i.kushch@ad.spetpro.ru>
2026-03-30 21:05:04 +1100

    Fix EU DST bug

--- frigate/output/preview.py
+++ frigate/output/preview.py
@@ -261,10 +261,9 @@ class PreviewRecorder:
             "v2": v2,
         }
 
-        # end segment at end of hour
+        # end segment at end of hour (use UTC to avoid DST issues)
         self.segment_end = (
-            (datetime.datetime.now() + datetime.timedelta(hours=1))
-            .astimezone(datetime.timezone.utc)
+            (datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(hours=1))
             .replace(minute=0, second=0, microsecond=0)
             .timestamp()
         )
@@ -274,8 +273,7 @@ class PreviewRecorder:
 
         # check for existing items in cache
         start_ts = (
-            datetime.datetime.now()
-            .astimezone(datetime.timezone.utc)
+            datetime.datetime.now(datetime.timezone.utc)
             .replace(minute=0, second=0, microsecond=0)
             .timestamp()
         )
@@ -309,8 +307,10 @@ class PreviewRecorder:
 
     def reset_frame_cache(self, frame_time: float) -> None:
         self.segment_end = (
-            (datetime.datetime.now() + datetime.timedelta(hours=1))
-            .astimezone(datetime.timezone.utc)
+            (
+                datetime.datetime.fromtimestamp(frame_time, tz=datetime.timezone.utc)
+                + datetime.timedelta(hours=1)
+            )
             .replace(minute=0, second=0, microsecond=0)
             .timestamp()
         )
