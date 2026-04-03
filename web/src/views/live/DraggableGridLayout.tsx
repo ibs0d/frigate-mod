560a6ce67fe2c770801b9bc9bbe18319027d988d
ibs0d <53568938+ibs0d@users.noreply.github.com>
2026-04-03 19:47:38 +1100

    Revert "Fix grid resize flicker by keying Responsive on gridCols"

--- web/src/views/live/DraggableGridLayout.tsx
+++ web/src/views/live/DraggableGridLayout.tsx
@@ -1080,7 +1080,7 @@ export default function DraggableGridLayout({
           />
           {containerWidth > 0 && fitToScreen && fitGridParams && (
             <Responsive
-              key={`fit-grid-${fitGridParams.gridCols}`}
+              key="fit-grid"
               className="grid-layout"
               width={availableWidth}
               compactor={noCompactor}
