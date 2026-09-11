1a911acc97039cfe38de5040360acb852b538447
Игорь Владимирович <i.kushch@ad.spetpro.ru>
2026-09-11 12:40:14 +1100

    Fullscreen for vahta

--- web/src/views/live/DraggableGridLayout.tsx
+++ web/src/views/live/DraggableGridLayout.tsx
@@ -53,7 +53,10 @@ import LiveContextMenu from "@/components/menu/LiveContextMenu";
 import { useStreamingSettings } from "@/context/streaming-settings-provider";
 import { useTranslation } from "react-i18next";
 import useKeyboardListener from "@/hooks/use-keyboard-listener";
-import { useIsVahtaRole } from "@/hooks/use-is-vahta-role";
+import {
+  isFitToScreenEnabled,
+  useIsVahtaRole,
+} from "@/hooks/use-is-vahta-role";
 import {
   CAMERA_ZOOM_MIN_SCALE,
   CameraZoomRuntimeTransform,
@@ -134,9 +137,13 @@ export default function DraggableGridLayout({
   const [gridLayout, setGridLayout, isGridLayoutLoaded] =
     useUserPersistence<Layout>(`${cameraGroup}-draggable-layout`);
 
-  const [fitToScreen, setFitToScreen] = useUserPersistence(
+  const [persistedFitToScreen, setFitToScreen] = useUserPersistence(
     `${cameraGroup}-fitToScreen`,
     true,
+  );
+  const fitToScreen = isFitToScreenEnabled(
+    isVahtaRole,
+    persistedFitToScreen,
   );
 
   const [fitCameraOrder, setFitCameraOrder] = useUserPersistence<string[]>(
@@ -486,7 +493,7 @@ export default function DraggableGridLayout({
     // Сбросить сохранённый порядок только если изменился набор камер
     // (добавили/удалили камеру), не при изменении размера окна
     setFitCameraOrder(undefined);
-  }, [cameraKey, includeBirdseye]);
+  }, [cameraKey, includeBirdseye, setFitCameraOrder]);
 
   const handleFitDragStop = useCallback(
     (
